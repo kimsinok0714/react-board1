c3177d7be21ef87a950b28ef46df0b152276c0ab
kimsinok0714 <javaenjoy0714@gmail.com>
2025-01-27 10:56:30 +0900

    Update BoardList.jsx

--- src/component/BoardList.jsx
+++ src/component/BoardList.jsx
@@ -24,7 +24,7 @@ const BoardList = () => {
 
     const [serverData, setServerData] = useState({...initialState});
 
-    const [keyfield, setKeyfield] = useState('writer');
+    const [keyfield, setKeyfield] = useState('');
 
     const [keyword, setKeyword] = useState('');
 
